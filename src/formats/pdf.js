184ff489e27d5b39894b91ca82ee1e2d3aff20f2
Kris Carle <kris@maphubs.com>
2018-06-20 18:27:03 -0400

    don't allow local file path

--- src/formats/pdf.js
+++ src/formats/pdf.js
@@ -2,11 +2,19 @@
 const pTimeout = require('p-timeout');
 import type {OptionsType} from '../types/options';
 
+// for options see https://pptr.dev/#?product=Puppeteer&version=v1.5.0&show=api-pagepdfoptions
+
 module.exports = async function(options: OptionsType, page: any, res: any){
   const defaultOptions = {
     printBackground: true
-  }
+  };
   const combinedOptions = Object.assign(defaultOptions, options);
+
+  // don't allow the PDF to be written to local disk
+  if(combinedOptions.path){
+    delete combinedOptions.path;
+  }
+
   const pdf = await pTimeout(page.pdf(combinedOptions), 30 * 1000, 'PDF timed out');
 
   res.writeHead(200, {
